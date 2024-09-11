fd0c69df42171d77d6b6bac9a0bbd1d008907f7e
Fatemeh Fathalian <mahdihamldar@gmail.com>
2024-09-11 10:33:55 +0330

    security

--- src/main/java/org/example/security/config/JwtService.java
+++ src/main/java/org/example/security/config/JwtService.java
@@ -1,11 +1,40 @@
 package org.example.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.PublicKey;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    public String extractUserName(String token) {
+    private static final String SECRET_KEY = "81cffb936f7da3dd8b7a66a825b5d51c8f58b5e63664cbcc8710d09d7f5a7550";
 
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
